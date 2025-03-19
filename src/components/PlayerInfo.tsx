9a6441facf9cafb47a21e420af96bbb3ff86af33
akayi <zhuolimiaotx@163.com>
2025-03-19 14:29:27 +0800

    add tip when user doesn't login apps

--- src/components/PlayerInfo.tsx
+++ src/components/PlayerInfo.tsx
@@ -8,17 +8,28 @@ export default function Commands() {
   return (
     <MDBContainer className="mt-3">
       <MDBRow>
-        {userState && userState.player && Object.entries(userState.player.data.positions).map(([tokenIndex, position]) => (
-          <MDBCol md="6" key={tokenIndex}>
+        {userState && userState.player ? (
+          Object.entries(userState.player.data.positions).map(([tokenIndex, position]) => (
+            <MDBCol md="6" key={tokenIndex}>
+              <MDBCard className="mb-3">
+                <MDBCardBody>
+                  <MDBTypography tag="h6" className="mb-2">Token {tokenIndex}</MDBTypography>
+                  <p className="mb-1"><strong>Balance:</strong> {position.balance}</p>
+                  <p className="mb-0"><strong>Locked:</strong> {position.lock_balance}</p>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          ))
+        ) : (
+          <MDBCol md="12">
             <MDBCard className="mb-3">
-              <MDBCardBody>
-                <MDBTypography tag="h6" className="mb-2">Token {tokenIndex}</MDBTypography>
-                <p className="mb-1"><strong>Balance:</strong> {position.balance}</p>
-                <p className="mb-0"><strong>Locked:</strong> {position.lock_balance}</p>
+              <MDBCardBody className="text-center">
+                <MDBTypography tag="h6" className="mb-2">No player available</MDBTypography>
+                <p>Please connect wallet and login apps to view balances.</p>
               </MDBCardBody>
             </MDBCard>
           </MDBCol>
-        ))}
+        )}
       </MDBRow>
     </MDBContainer>
   )
